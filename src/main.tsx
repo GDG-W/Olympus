1d509e4d47033db33d43c3b89c88ab64ef140ebf
coded1guy <grggmmnl@gmail.com>
2024-11-10 16:05:40 +0100

    feat: bootstrapped with react vite

---
+++ src/main.tsx
@@ -0,0 +1,23 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App.tsx";
+import { Login } from "./pages";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/me",
+    element: <App />,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
